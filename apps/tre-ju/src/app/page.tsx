06f333cfa2cebb3c7381395a6f580fdfd2a9b4f9
Marek Hanzal <marek.hanzal@x32.cz>
2024-03-02 20:53:14 +0100

    Preparing styles, some experiments

--- apps/tre-ju/src/app/page.tsx
+++ apps/tre-ju/src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
 	return (
-		<main className={"flex min-h-screen flex-col items-center justify-between p-24"}>
+		<main
+			className={"flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"}
+		>
 			<div className={"z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"}>
 				<p>
 					Get started by editing&nbsp;
